50b0b2f1c01d48431e93305330bc0a154545816f
alifhs <alif.hs@outlook.com>
2021-08-27 17:06:48 +0600

    added search option

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import "./assets/main.css";
+import { useState, useEffect } from "react";
+import { ImageCard } from "./components/imageCard";
+import { ImageSearch } from "./components/imageSearch";
+
+require("dotenv").config();
 
 function App() {
+  const [images, setImages] = useState([]);
+  const [isLoading, setIsloading] = useState(true);
+  const [term, setTerm] = useState("");
+
+  useEffect(() => {
+    fetch(
+      `https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_API_KEY}&q=${term}&image_type=photo`
+    )
+      .then((res) => res.json())
+      .then((data) =>{
+        setImages(data.hits)
+        setIsloading(false);
+      })
+      .catch((err) => console.log(err));
+  }, [term]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mx-auto">
+    <ImageSearch setTerm={setTerm}/>
+    {isLoading ? <h1 className="text-6xl text-center mx-auto mt-32">Loading....</h1> :
+      <div className="grid grid-cols-3 gap-4">
+        {images.map((image) => {
+          return  <ImageCard key={image.id} image={image}/>
+         
+        })}
+      </div>
+    }
     </div>
   );
 }
