772a2affd9039adfbc3e3c083e9eef7cc63a95a5
alifhs <alif.hs@outlook.com>
2022-04-21 20:11:00 +0600

    add async await

--- src/App.js
+++ src/App.js
@@ -10,23 +10,42 @@ function App() {
   const [isLoading, setIsloading] = useState(true);
   const [term, setTerm] = useState("");
 
+
   useEffect(() => {
-    fetch(
-      `https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_API_KEY}&q=${term}&image_type=photo`
-    )
-      .then((res) => res.json())
-      .then((data) =>{
-        setImages(data.hits)
-        setIsloading(false);
-      })
-      .catch((err) => console.log(err));
+
+    async function callPixabay(){
+
+      try {
+
+        const res = await fetch(
+           `https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_API_KEY}&q=${term}&image_type=photo`
+         )
+   
+         const data = await res.json();
+   
+         setImages(data.hits);
+         setIsloading(false);
+      } catch (err) {
+        console.log(err);
+      }
+
+
+    }
+    callPixabay();
+    
+      
   }, [term]);
+
+
+
+
+
 
   return (
     <div className="container mx-auto">
     <ImageSearch setTerm={setTerm}/>
     {isLoading ? <h1 className="text-6xl text-center mx-auto mt-32">Loading....</h1> :
-      <div className="grid grid-cols-3 gap-4">
+      <div className="grid grid-cols-3 gap-4" style={{ marginLeft : "25px" }}>
         {images.map((image) => {
           return  <ImageCard key={image.id} image={image}/>
          
@@ -38,3 +57,11 @@ function App() {
 }
 
 export default App;
+
+
+
+
+
+
+
+
